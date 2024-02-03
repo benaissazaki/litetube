dc26e34b3f67b771faa23daacf3ce26261ebfb36
Zaki Benaissa <zakben45@gmail.com>
2024-02-04 00:16:45 +0100

    feat: create search result view

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import search_view
+from .views import search_view, result_view
 
-urlpatterns = [path("", search_view, name="search")]
+urlpatterns = [
+    path("", search_view, name="search"),
+    path("result", result_view, name="result"),
+]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import pytube
 from .forms import VideoSearchForm
 
 
@@ -7,3 +7,12 @@ def search_view(request):
     context = {"video_search_form": VideoSearchForm()}
 
     return render(request, "search.html", context)
+
+
+def result_view(request):
+    search_query = request.GET.get("query")
+    search = pytube.Search(search_query)
+
+    context = {"search_results": search.results}
+
+    return render(request, "result.html", context)
