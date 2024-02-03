54505805aeef393ae233df8c3d9a8e8621a3a860
Zaki Benaissa <zakben45@gmail.com>
2024-02-03 22:52:08 +0100

    feat: create search page

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,5 @@
-urlpatterns = []
+from django.urls import path
+
+from .views import search_view
+
+urlpatterns = [path("", search_view, name="search")]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import VideoSearchForm
+
+
+def search_view(request):
+    context = {"video_search_form": VideoSearchForm()}
+
+    return render(request, "search.html", context)
