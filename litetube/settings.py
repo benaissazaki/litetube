a859eb9245ff7e604e42126b12276c2730aba607
Zaki Benaissa <zakben45@gmail.com>
2024-02-05 23:12:53 +0100

    ci: add vercel deploy settings

--- litetube/settings.py
+++ litetube/settings.py
@@ -128,8 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-if not DEBUG:
-    STATIC_ROOT = env('STATIC_ROOT')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- litetube/urls.py
+++ litetube/urls.py
@@ -24,4 +24,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
-] + static(settings.STATIC_URL)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
